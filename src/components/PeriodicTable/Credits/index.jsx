ceb32c470f4903b229a113e14128cc077841d6a4
cambo2015 <cameronhansen91@outlook.com>
2022-07-16 10:46:11 -0600

    deleted images

--- src/components/PeriodicTable/Credits/index.jsx
+++ src/components/PeriodicTable/Credits/index.jsx
@@ -11,6 +11,11 @@ const Credits = () => {
         </a>{" "}
         for the images and text summary content.
       </span>
+      <div className="text-center p-2">
+        <button className="btn btn-primary" style={{ width: 100 }}>
+          <i class="fa-brands fa-github"></i> &nbsp;Github
+        </button>
+      </div>
     </div>
   );
 };
