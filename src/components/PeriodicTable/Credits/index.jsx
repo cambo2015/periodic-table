74519b9aa3ec4626b20ff9ddde2245cdd9139fb4
cambo2015 <cameronhansen91@outlook.com>
2022-07-16 11:17:49 -0600

    edited credits to include license, code on github for myself and Browserinator

--- src/components/PeriodicTable/Credits/index.jsx
+++ src/components/PeriodicTable/Credits/index.jsx
@@ -3,17 +3,36 @@ import React from "react";
 const Credits = () => {
   return (
     <div class="card card-body" id="credits">
-      Made by Cameron Hansen Copyright 2022. <br />
+      Made by Cameron Hansen <br />
       <span className="small-text">
-        Many thanks to{" "}
+        Thanks to{" "}
         <a target="_blank" href="https://wikipedia.com" rel="noreferrer">
           wikipedia
         </a>{" "}
-        for the images and text summary content.
+        for the images and text summary content
+      </span>
+      <span className="small-text">
+        and to &nbsp;
+        <a href="https://github.com/Bowserinator/Periodic-Table-JSON">
+          Browserinator
+        </a>
+        &nbsp;for the json data.
+      </span>
+      <span className="small-text">
+        License is{" "}
+        <a href="https://creativecommons.org/licenses/by-sa/3.0">
+          Creative Commons Attribution-ShareAlike 3.0 Unported
+        </a>
       </span>
       <div className="text-center p-2">
-        <button className="btn btn-primary" style={{ width: 100 }}>
-          <i class="fa-brands fa-github"></i> &nbsp;Github
+        <button className="btn btn-primary" style={{ width: 300 }}>
+          <i class="fa-brands fa-github"></i> &nbsp;
+          <a
+            href="https://github.com/cambo2015/periodic-table"
+            className="text-white"
+          >
+            View the code on Github
+          </a>
         </button>
       </div>
     </div>
